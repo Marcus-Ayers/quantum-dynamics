13cd5eb826af401c8084a34c814c66dc955240c7
Marcus-Ayers <marcayers27@gmail.com>
2023-06-19 11:42:56 -0400

    remove react-router

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 import {
   About,
@@ -15,26 +15,26 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div
-          className="bg-hero-pattern bg-cover bg-no-repeat bg-center"
-          // style={{ backgroundImage: "/../public/images/black.jpeg" }}
-        >
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Tech />
-        <Works />
-        <Experience />
-        {/* <Feedbacks /> */}
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
+    // <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div
+        className="bg-hero-pattern bg-cover bg-no-repeat bg-center"
+        // style={{ backgroundImage: "/../public/images/black.jpeg" }}
+      >
+        <Navbar />
+        <Hero />
       </div>
-    </BrowserRouter>
+      <About />
+      <Tech />
+      <Works />
+      <Experience />
+      {/* <Feedbacks /> */}
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+    // </BrowserRouter>
   );
 };
 
--- public/components/components/Navbar.jsx
+++ public/components/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
@@ -34,24 +34,24 @@ const Navbar = () => {
       }`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
+        {/* <Link
           to="/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}
-        >
-          <img
-            src="../../images/logoColor.svg"
-            alt="logo"
-            className="w-9 h-9 object-contain"
-          />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Marcus &nbsp;
-            {/* <span className='sm:block hidden'> | JavaScript Mastery</span> */}
-          </p>
-        </Link>
+        > */}
+        <img
+          src="../../images/logoColor.svg"
+          alt="logo"
+          className="w-9 h-9 object-contain"
+        />
+        <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+          Marcus &nbsp;
+          {/* <span className='sm:block hidden'> | JavaScript Mastery</span> */}
+        </p>
+        {/* </Link> */}
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((nav) => (
